4ec3c6af3bd6bad7e32f741729388acd28eb6f72
Matheus Cavalcante <mc2798353@gmail.com>
2024-01-17 16:04:59 -0300

    Estudo de Modulos em Python, fazendo a primeira modulação durante os estudos

---
+++ PYTHON_MOD/1-modulos/calc.py
@@ -0,0 +1,15 @@
+def sum(a,b):
+    result = a + b
+    return result
+
+def sub(a,b):
+    result = a - b
+    return result
+    
+def mut(a,b):
+    result = a * b
+    return result
+
+def div(a,b):
+    result = a / b
+    return result
