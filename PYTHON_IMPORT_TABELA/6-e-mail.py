1a50c72e46bcab723bf02939485014a27f94f040
Matheus Cavalcante <mc2798353@gmail.com>
2024-02-14 08:55:06 -0300

    Atualização

--- PYTHON_IMPORT_TABELA/6-e-mail.py
+++ PYTHON_IMPORT_TABELA/6-e-mail.py
@@ -2,33 +2,33 @@ import smtplib
 import ssl
 import mimetypes
 from email.message import EmailMessage
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 # 1 - Dados do E-mail
-password = open('senha', 'r').read()
+password = os.getenv("PASSWORD")
 from_email = "mc2798353@gmail.com"
 to_email = "mc2798353@gmail.com"
 subject = "Automação Planilha"
 body = """
 Olá. Segue em Anexo a Automação da Planilha, 
 Para a Empresa XYZ Automação.
-
 Qualquer dúvida estou a disposição
 """
-
 # 2 - Montando a estrutura do e-mail
 message = EmailMessage()
 message["From"] = from_email
 message["To"] = to_email
 message["Subject"] = subject
-
 message.set_content(body)
 safe = ssl.create_default_context()
-
 # 3 - Adicionar Anexo
 anexo = "data/relatório.xlsx"
 mime_type, mime_subtype = mimetypes.guess_type(anexo)[0].split('/')
 with open(anexo, 'rb') as a:
-    message.add_attachment(
+     message.add_attachment(
         a.read(),
         maintype=mime_type,
         subtype=mime_subtype,
--- PYTHON_MOD/7-statistic/7-statistic.py
+++ PYTHON_MOD/7-statistic/7-statistic.py
@@ -10,3 +10,6 @@ print(statistics.median([1, 2, 3, 5, 8, 6, 8, 9]))
 
 # 3 - Aplicar a moda
 print(statistics.mode([0,2,2,0,0,2,6,5,5,6,6,7,7,9,9,4,4,1,5,4,5,4,2,2,3]))
+
+# 4  - Desvio padrão
+print(statistics.stdev([1,1.5,2,2.5,3,3.5,4,4.5]))
