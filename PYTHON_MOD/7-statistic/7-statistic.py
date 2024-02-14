5cedde9c4d6c6b7a8327e09c7f3eb766a1a77266
Matheus Cavalcante <mc2798353@gmail.com>
2024-02-14 08:19:06 -0300

    Utilização de módulo de estatistica em Python, realizando média, mediana, moda e desvio padrão

---
+++ PYTHON_MOD/7-statistic/7-statistic.py
@@ -0,0 +1,12 @@
+import statistics
+
+# 1 - Aplicar a média
+
+print(statistics.mean([3,2,5,6,8,4,0,9]))
+
+
+# 2 - Aplicar médiana
+print(statistics.median([1, 2, 3, 5, 8, 6, 8, 9]))
+
+# 3 - Aplicar a moda
+print(statistics.mode([0,2,2,0,0,2,6,5,5,6,6,7,7,9,9,4,4,1,5,4,5,4,2,2,3]))
