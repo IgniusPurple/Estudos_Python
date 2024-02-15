91014ad2eb8933612497f1286cf48a6617a0041c
Matheus Cavalcante <mc2798353@gmail.com>
2024-02-15 11:40:17 -0300

    PYTHON_MOD/10-hashlib/

---
+++ PYTHON_MOD/10-hashlib/10-hashlib.py
@@ -0,0 +1,20 @@
+import hashlib
+
+# 1 - Verificar os algoritmos disponiveis
+print(hashlib.algorithms_available)
+
+# 2 - Verificar os algoritmos disponiveis de acordo com o SO
+print(hashlib.algorithms_guaranteed)
+
+# 3- Utilizando Sha256
+algorithm = hashlib.sha256()
+print(algorithm.digest())
+message = "A melhor forma de prever o futuro é criá-lo".encode()
+algorithm.update(message)
+print(algorithm.hexdigest())
+
+
+# 4 -  Utilizando o MD5
+md5 = hashlib.md5()
+md5.update(message)
+print(md5.hexdigest())
